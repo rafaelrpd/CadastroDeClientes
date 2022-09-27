ecc21d5677edcd568fdae24d18397fbaf26be67e
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-27 11:52:03 -0300

    criado os modelos de cidade e estado da api

---
+++ WebAPI/Models/Cidade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    [Table("cidades")]
+    public partial class Cidade
+    {
+        public Cidade()
+        {
+            Clientes = new HashSet<Cliente>();
+        }
+
+        [Key]
+        [Column("cidadeId")]
+        public int CidadeId { get; set; }
+        [Column("nome")]
+        [Unicode(false)]
+        public string Nome { get; set; } = null!;
+        [Column("estadoId")]
+        public int EstadoId { get; set; }
+
+        [ForeignKey("EstadoId")]
+        [InverseProperty("Cidades")]
+        public virtual Estado Estado { get; set; } = null!;
+        [InverseProperty("Cidade")]
+        public virtual ICollection<Cliente> Clientes { get; set; }
+    }
+}
---
+++ WebAPI/Models/Estado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    [Table("estados")]
+    public partial class Estado
+    {
+        public Estado()
+        {
+            Cidades = new HashSet<Cidade>();
+        }
+
+        [Key]
+        [Column("estadoId")]
+        public int EstadoId { get; set; }
+        [Column("nome")]
+        [Unicode(false)]
+        public string Nome { get; set; } = null!;
+        [Column("sigla")]
+        [StringLength(2)]
+        [Unicode(false)]
+        public string Sigla { get; set; } = null!;
+
+        [InverseProperty("Estado")]
+        public virtual ICollection<Cidade> Cidades { get; set; }
+    }
+}
