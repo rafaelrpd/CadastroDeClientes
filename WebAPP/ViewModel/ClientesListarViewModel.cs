a4894a43491f636dc5b14613341a4e803c2c49e4
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-27 18:16:33 -0300

    adicionado 2 novas propriedades a ViewModel ClientesListar a fim de ajudar no formulário de pesquisa da view Index de clientes.

--- WebAPP/ViewModel/ClientesListarViewModel.cs
+++ WebAPP/ViewModel/ClientesListarViewModel.cs
@@ -1,9 +1,12 @@
-﻿using WebAPP.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebAPP.Models;
 
 namespace WebAPP.ViewModel
 {
     public class ClientesListarViewModel
     {
         public IEnumerable<Cliente> Clientes { get; set; }
+        public SelectList Cidades { get; set; }
+        public SelectList Estados { get; set; }
     }
 }
